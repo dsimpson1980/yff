8b8c7699177500a6edd8072658cd0c37cfed5d73
davidsimpson <mapdes@gmail.com>
2015-09-19 16:00:30 +0100

    Standardised some of the config extraction methods

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def set_default_config():
     for section, keys in parameters.iteritems():
         config.add_section(section)
         for key in keys:
-            config.set(section, key)
+            config.set(section, key, ' ')
     with open(default, 'w') as file:
         config.write(file)
 
@@ -38,15 +38,6 @@ def config_map(section, key):
         value = None
     return value
 
-def get_yahoo_username(query=None):
-    username = config_map('Yahoo', 'username')
-    if username == '':
-        if query is None:
-            username = raw_input('Enter %s' % 'username')
-        else:
-            username = query()
-    return username
-
 def get_consumer_secret(query=None):
     consumer = config_map('Yff', 'consumer')
     secret = config_map('Yff', 'secret')
@@ -57,6 +48,21 @@ def get_consumer_secret(query=None):
         else:
             consumer, secret = query()
     return consumer, secret
+
+def get_yahoo_username(query=None):
+    return get_general_one_parameter(query, 'Yahoo', 'username')
+
+def get_league(query=None):
+    return get_general_one_parameter(query, 'Fantasy', 'league')
+
+def get_league_number(query=None):
+    return get_general_one_parameter(query, 'Fantasy', 'league_number')
+
+def get_general_one_parameter(query, section, key):
+    value = config_map(section, key)
+    if value == '':
+        value = raw_input('Enter %s:' % key) if query is None else query()
+    return value
 
 if os.path.exists(default):
     config.read(default)
